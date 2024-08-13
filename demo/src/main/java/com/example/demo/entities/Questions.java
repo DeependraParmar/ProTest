e18e96765eea2a6ef9033a6a0d64bb9f5bf01ed6
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-08-13 09:42:36 +0530

    Added Questions Entity and the tables got created successfully

---
+++ demo/src/main/java/com/example/demo/entities/Questions.java
@@ -0,0 +1,44 @@
+package com.example.demo.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "questions")
+public class Questions {
+    @Id
+    @Column(name = "question_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "option1", nullable = false)
+    private String option1;
+
+    @Column(name = "option2", nullable = false)
+    private String option2;
+
+    @Column(name = "option3", nullable = false)
+    private String option3;
+
+    @Column(name = "option4", nullable = false)
+    private String option4;
+
+    @Column(name = "correct_answer", nullable = false)
+    private Integer rightAnswer;
+
+    @ManyToOne
+    @JoinColumn(name = "test")
+    private Test test;
+}
---
+++ demo/src/main/java/com/example/demo/entities/Test.java
@@ -0,0 +1,34 @@
+package com.example.demo.entities;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name = "tests")
+@NoArgsConstructor
+@AllArgsConstructor
+public class Test {
+    @Id
+    @Column(name = "test_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "test_date", nullable = false)
+    private LocalDate testDate;
+
+    @Column(name = "post_date", nullable = false)
+    private LocalDate postDate;
+
+    @Column(name = "status", nullable = false)
+    private Boolean status;
+}
