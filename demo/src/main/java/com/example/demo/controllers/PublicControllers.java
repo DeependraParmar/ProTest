2248002d2e8bc11b8f5b8fe0e8ee5242ce46db93
Deependra Parmar <104254575+DeependraParmar@users.noreply.github.com>
2024-09-02 09:11:53 +0530

    Added something more in authentication

--- demo/src/main/java/com/example/demo/controllers/PublicControllers.java
+++ demo/src/main/java/com/example/demo/controllers/PublicControllers.java
@@ -1,6 +1,10 @@
 package com.example.demo.controllers;
 
+import java.util.HashMap;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -21,14 +25,12 @@ import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-
-
 @RestController
 @RequestMapping("/public")
 public class PublicControllers extends BaseController {
     @Autowired
     private StudentService studentService;
-    
+
     @Autowired
     private FacultyService facultyService;
 
@@ -38,12 +40,23 @@ public class PublicControllers extends BaseController {
     @Autowired
     private PasswordEncoder encoder;
 
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    private HashMap<String, String> roles;
+
+    public PublicControllers() {
+        roles = new HashMap<>();
+        roles.put("ROLE_ADMIN", "admin");
+        roles.put("ROLE_FACULTY", "faculty");
+        roles.put("ROLE_STUDENT", "student");
+    }
+
     // Test Mapping for public routes
     @GetMapping("")
     public String hello() {
         return new String("Hello from public controller");
     }
-    
 
     // Post mapping for student registration
     @PostMapping("/register/student")
@@ -54,23 +67,28 @@ public class PublicControllers extends BaseController {
 
     // Post mapping for faculty registration
     @PostMapping("/register/faculty")
-    public ApiResponse registerFaculty(@Valid @RequestBody FacultyRegister object){
+    public ApiResponse registerFaculty(@Valid @RequestBody FacultyRegister object) {
         object.setPassword(encoder.encode(object.getPassword()));
         return facultyService.register(object);
     }
-    
+
     // Post mapping for login
     @PostMapping("/login")
     public ApiResponse login(@Valid @RequestBody LoginModel object) {
-        User user = userService.checkLogin(object);
+        try {
+            
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(object.getEmail(), object.getPassword()));
+            User user = (User) userService.loadUserByUsername(object.getEmail());
 
-        if(user == null){
-            return new ApiResponse(false,null, "Invalid User, Login Failed!", null);
-        }
-        else{
-            LoginResponse response = new LoginResponse(user.getName(), null, user.getRole());
-            return new ApiResponse(true,response, "Login Success", null);
+            if (user == null) {
+                return new ApiResponse(false, null, "Invalid User, Login Failed!", null);
+            } else {
+                LoginResponse response = new LoginResponse(user.getName(), null, user.getRole());
+                return new ApiResponse(true, response, "Login Success", null);
+            }
+        } catch (Exception ex) {
+            return new ApiResponse(false, null, "Invalid User, Login Failed!", null);
         }
     }
-    
+
 }
